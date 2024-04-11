ae5c8ef50328b7f7f9be91b53bb5a4e1b5e47a7b
Yu <e2296635@cmaisonneuve.qc.ca>
2024-04-11 17:26:55 -0400

    modifier la page de connexion
    le <label> doit être à gouche

--- concessionaire/resources/views/user/login.blade.php
+++ concessionaire/resources/views/user/login.blade.php
@@ -6,10 +6,14 @@
             @csrf
             <img class="mb-4" src="{{asset('img/logo.png')}}" alt="" width="176" height="155">
             <h1 class="h3 mb-3 font-weight-normal">Connectez-vous à votre compte</h1>
-            <label for="inputEmail" class="sr-only form-label">Courriel</label>
-            <input name="courriel" type="email" id="inputEmail" class="form-control" placeholder="Courriel*" required autofocus>
-            <label for="inputPassword" class="sr-only form-label">Mot de passe</label>
-            <input name="mdp" type="password" id="inputPassword" class="form-control" placeholder="Mot de passe*" required>
+            <div class="form-group mb-3 text-start">
+                <label for="inputEmail" class="sr-only form-label">Courriel</label>
+                <input name="courriel" type="email" id="inputEmail" class="form-control" placeholder="Courriel*" required autofocus>
+            </div>
+            <div class="form-group mb-3 text-start">
+                <label for="inputPassword" class="sr-only form-label">Mot de passe</label>
+                <input name="mdp" type="password" id="inputPassword" class="form-control" placeholder="Mot de passe*" required>
+            </div>
             <p><a href="#" class="link-underline-primary">Mot de passe oublié ?</a></p>
             <button class="btn btn-lg btn-primary w-100" type="submit">Se connecter</button>
         </form>
