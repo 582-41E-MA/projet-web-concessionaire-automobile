fd6b626b11b2c2f61148de378c91e755eb819d02
Yu <e2296635@cmaisonneuve.qc.ca>
2024-04-19 10:33:16 -0400

    fixer html

--- concessionaire/resources/views/layouts/app.blade.php
+++ concessionaire/resources/views/layouts/app.blade.php
@@ -63,13 +63,14 @@
     @endif
     <!--main-->
     <div>
-            <div class="container my-5">
+        <div class="container my-5">
             @if(session('success'))
             <div class="alert alert-success alert-dismissible fade show" role="alert">
                 {{ session('success')}}
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
             @endif
+        </div>
         @yield('content')
     </div>
     <!--footer-->
