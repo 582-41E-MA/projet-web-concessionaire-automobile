0d95baa6a04ee5b74e061468073c74960650a60b
vilain-petit-canard <lilkaibra@gmail.com>
2024-04-21 08:17:36 -0400

    editer et supprimer une voiture dans le controller, fonctions ecrites

--- concessionaire/app/Http/Controllers/VoitureController.php
+++ concessionaire/app/Http/Controllers/VoitureController.php
@@ -112,4 +112,88 @@ class VoitureController extends Controller
         return view('voiture.show', ["voiture" => $voiture]);
     }
 
+    
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Voiture $voiture)
+    {
+        //
+        $pays = Pays::all();
+        $photos = Photo::all();
+        $carrosseries = Carrosserie::all();
+        $marques = Marque::all();
+        
+        return view('voiture.edit', ["photos" => $photos, "pays" => $pays, "carrosseries" => $carrosseries, "marques" => $marques ]);
+    }
+
+     /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Voiture $voiture)
+    {
+        $request->validate([
+            'photos' => 'required',
+            'description_fr' => 'required|string',
+            'description_en' => 'required|string',
+            'annee' => 'required|numeric',
+            'date_arrivee' => 'required|date',
+            'prix_base' => 'required|numeric',
+            'taux_augmenter' => 'required|numeric|min:0|max:100',
+            'prix_paye' => 'required|numeric',
+            'carrosserie' => 'required|numeric',
+            'marque' => 'required|numeric',
+            'modele' => 'required|numeric',
+            'pays' => 'required|numeric',
+        ]);
+
+        $voiture = Voiture::update([
+            'description_fr' => $request->description_fr,
+            'description_en' => $request->description_en,
+            'annee' => $request->annee,
+            'date_arrivee' => $request -> date_arrivee,
+            'prix_base' => $request->prix_base,
+            'taux_augmenter' => $request->taux_augmenter,
+            'prix_paye' => $request->prix_paye,
+            'modele_id' => $request->modele,
+            'carrosserie_id' => $request->carrosserie,
+            'employe_id' => Auth::id(),
+            'pays_id' => $request->pays,
+            'commande_id' => null,
+        ]);
+
+        $photosArray[] = $request -> photos;
+     
+        foreach ($photosArray as $key => $photo) {
+
+            foreach ($photo as $key => $singlePhoto) {
+                $picName = $singlePhoto->getClientOriginalName();
+                $singlePhoto -> move(public_path('images'), $picName);
+
+                $photosVoiture = Photo::create([
+                        'photo_titre' => $picName,
+                        'photo_voiture_id' => $voiture ->id
+                    ]);
+            }
+        }
+        // créer des dossier automatiquement des dossiers dans image, qui ont le meme nom que le id de la voiture, pour faciliter la gestion et eviter du overwritting ou des noms dupliqués
+        // et prendre en compte ce qui se passe quand une photo uploadée dans public, a le meme nom qu'une photo deja presente
+        // mettre le array dans photo selectioné dans un autre dans js peut-etre
+        // il faut peut-etre avoir un controlleur separé pour photo, pour l'ajout et la suppresion de photo
+        // et chercher comment conserver les photos initialement uploadées, lorsqu'on clique sur upload une deuxieme fois. 
+
+        return  redirect()->route('voiture.show', $voiture->id)->with('success', 'voiture created successfully!');
+
+    }
+
+     /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Voiture $voiture)
+    {
+        //
+        $voiture->delete();
+        return redirect()->route('accueil')->with('success', 'voiture supprimee avec succes');
+    }
+
 }
