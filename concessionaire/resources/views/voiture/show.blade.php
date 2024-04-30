f42a7d45611856d34f76d4329707e8803d8223ab
saif-eddine-baklouti <saif.eddine.baklouti@gmail.com>
2024-04-30 15:57:34 -0400

    ajuster le conflit dans show voiture

--- concessionaire/resources/views/voiture/show.blade.php
+++ concessionaire/resources/views/voiture/show.blade.php
@@ -53,9 +53,7 @@
           <hr class="mb-4" />
 
           <a href="#" class="btn btn-info shadow-0">@lang('Buy now')</a>
-          <a href="#" class="btn btn-primary shadow-0"> <i class="me-1 fa fa-shopping-basket"></i>@lang('Add to cart')</a>
           <a href="#" class="btn btn-light border border-secondary py-2 icon-hover px-3"> <i class="me-1 fa fa-heart fa-lg"></i>@lang('Reserve')</a>
-          <a href="#" class="btn btn-info shadow-0"> Acheter maintenant </a>
           <form action="{{ route('panier.store') }}" method="post">
             @csrf
               <input type="hidden" name="voiture_id" value="{{ $voiture->id }}">
@@ -65,9 +63,8 @@
               <input type="hidden" name="prix" value="{{$voiture->prix_paye}}" >
               <input type="hidden" name="province_user_id" value="{{ Auth::user()->province_id }}" >
               <input type="hidden" name="user_id" value="{{ Auth::user()->id }} ">
-            <button type="submit" class="btn btn-primary shadow-0 border-0 me-1 fa fa-shopping-basket"> Ajouter au panier </button>
+            <button type="submit" class="btn btn-primary shadow-0 border-0 me-1 fa fa-shopping-basket"> @lang('Add to cart') </button>
           </form>
-          <a href="#" class="btn btn-light border border-secondary py-2 icon-hover px-3"> <i class="me-1 fa fa-heart fa-lg"></i> Réserver </a>
         </div>
       </section>
     </div>
